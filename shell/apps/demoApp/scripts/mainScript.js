b61d20bdcf5b0fc2e21e0c199119732b910054f7
swervo <swervo@papersnail.co.uk>
2010-11-01 21:36:12 +0000

    Clean up, Change to copy in index.html

--- shell/apps/demoApp/scripts/mainScript.js
+++ shell/apps/demoApp/scripts/mainScript.js
@@ -11,6 +11,8 @@ var demoApp = {
     connectUp: function() {
         var toArray = function (obj) {
             var array = [];
+            // taken from solution offerred by CMS - http://stackoverflow.com/users/5445/cms
+            // to question - http://stackoverflow.com/questions/2735067/
             // iterate backwards ensuring that length is an UInt32
             for (var i = obj.length >>> 0; i--;) {
                 array[i] = obj[i];
